70bc759e6125ea6e08952335e64539c2f07979e9
Mohamed Elalem <mohamed.el.alem.2017@gmail.com>
2020-08-28 11:18:25 -0500

    Add current_page and total_pages to reviews response

--- spec/requests/reviews_request_spec.rb
+++ spec/requests/reviews_request_spec.rb
@@ -12,14 +12,18 @@ RSpec.describe "Reviews", type: :request do
       get product_reviews_path(product), headers: Helpers::Constants::HEADERS
       expect(response).to have_http_status :success
       parsed_body = JSON.parse(response.body)
-      expect(parsed_body['data'].length).to eq(5)  
+      expect(parsed_body['data']['reviews'].length).to eq(5)
+      expect(parsed_body['data']['current_page']).to eq(1)
+      expect(parsed_body['data']['total_pages']).to eq(2)
     end
 
     it 'retrieves second reviews page' do
       get product_reviews_path(product, page: 2), headers: Helpers::Constants::HEADERS
       expect(response).to have_http_status :success
       parsed_body = JSON.parse(response.body)
-      expect(parsed_body['data'].length).to eq(1)
+      expect(parsed_body['data']['reviews'].length).to eq(1)
+      expect(parsed_body['data']['current_page']).to eq(2)
+      expect(parsed_body['data']['total_pages']).to eq(2)
     end
   end
 end
